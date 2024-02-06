7a6f8f2b0cce9315c01429bc201ff82ed829c797
lucasabdouni <lucasabdouni@hotmail.com>
2024-02-06 13:01:15 -0300

    feat: created toolbar component

---
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-toolbar-navigation',
+  templateUrl: './toolbar-navigation.component.html',
+  styleUrls: [],
+})
+export class ToolbarNavigationComponent {
+  constructor(private router: Router, private cookieService: CookieService) {}
+
+  handleLogout(): void {
+    this.cookieService.delete('USER_INFO');
+    void this.router.navigate(['/home']);
+  }
+}
