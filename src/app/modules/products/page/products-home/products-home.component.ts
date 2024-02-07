8d1f884f34124ef5f3be1a017666a4133de6012e
lucasabdouni <lucasabdouni@hotmail.com>
2024-02-07 13:04:00 -0300

    feat: created router and component produts

---
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { ProductDataTransferService } from './../../../../shared/services/products/product-data-transfer.service';
+
+@Component({
+  selector: 'app-products-home',
+  templateUrl: './products-home.component.html',
+  styleUrls: [],
+})
+export class ProductsHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  public productsDatas: Array<GetAllProductsResponse> = [];
+
+  constructor(
+    private productService: ProductsService,
+    private productsDtService: ProductDataTransferService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
+  ngOnInit(): void {
+    this.getServiceProductsDatas();
+  }
+
+  getServiceProductsDatas() {
+    const productsLoaded = this.productsDtService.getProductsDatas();
+
+    if (productsLoaded.length > 0) {
+      this.productsDatas = productsLoaded;
+    } else {
+      this.getAPIProductsDatas();
+    }
+  }
+  getAPIProductsDatas() {
+    this.productService
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsDatas = response;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buiscar produtos',
+            life: 2500,
+          });
+          this.router.navigate(['/dashboard']);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
